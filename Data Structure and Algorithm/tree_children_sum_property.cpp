9f2c5b9dd0c66076dc4498fda38dd07a3593e0f0
Aarti Rathi <91717327+codewithrathi@users.noreply.github.com>
2022-10-09 13:09:28 +0530

    Create tree_children_sum_property.cpp

---
+++ Data Structure and Algorithm/tree_children_sum_property.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node  
+{ 
+  int key; 
+  struct Node *left; 
+  struct Node *right; 
+  Node(int k){
+      key=k;
+      left=right=NULL;
+  }
+};
+
+bool isCSum(Node *root){
+    if(root==NULL)
+        return true;
+    if(root->left==NULL && root->right==NULL)
+        return true;
+    int sum=0;
+    if(root->left!=NULL)sum+=root->left->key;
+    if(root->right!=NULL)sum+=root->right->key;
+    
+    return (root->key==sum && isCSum(root->left) && isCSum(root->right));
+}
+
+int main() {
+	
+	Node *root=new Node(20);
+	root->left=new Node(8);
+	root->right=new Node(12);
+	root->right->left=new Node(3);
+	root->right->right=new Node(9);
+	
+	cout<<isCSum(root);
+}
